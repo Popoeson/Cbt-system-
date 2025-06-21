ab1ab99b011e136345c8bd4935a9ff291642c7e1
Samuel Popoola <samuelpopoola323@gmail.com>
2025-06-21 18:14:09 +0100

    Update cbt-server.cjs

--- cbt-server.cjs
+++ cbt-server.cjs
@@ -6,7 +6,6 @@ const fs = require("fs");
 const path = require("path");
 const csv = require("csv-writer");
 const router = express.Router();
-const Admin = require('../models/Admin');
 
 
 const app = express();
