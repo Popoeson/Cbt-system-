527039c599adf7e2ce7dd17a0d94ab0cf8e4ce73
Samuel Popoola <samuelpopoola323@gmail.com>
2025-06-22 23:18:20 +0100

    Update cbt-server.cjs

--- cbt-server.cjs
+++ cbt-server.cjs
@@ -308,7 +308,6 @@ app.post("/api/students/login", async (req, res) => {
 
 // Upload Scheduled Students via Excel (POST)
 app.post("/api/schedule/upload", scheduleUpload.single("file"), async (req, res) => {
-});
   try {
     const workbook = XLSX.read(req.file.buffer);
     const sheetName = workbook.SheetNames[0];
